0ee9ac4bcd9d1f70b6cedd74788e24d0edbd5667
MarcoHernandez02 <marco_1320114125@uptecamac.edu.mx>
2022-01-25 19:14:02 -0600

    Blog personal Cliente Servidor

---
+++ blog/resources/views/Blog/project.blade.php
@@ -0,0 +1,187 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta content="IE=edge" http-equiv="X-UA-Compatible">
+  <meta content="width=device-width,initial-scale=1" name="viewport">
+  <meta content="description" name="description">
+  <meta name="google" content="notranslate" />
+  <meta content="Mashup templates have been developped by Orson.io team" name="author">
+
+  <!-- Disable tap highlight on IE -->
+  <meta name="msapplication-tap-highlight" content="no">
+  
+  <link rel="apple-touch-icon" sizes="180x180" href="./images/apple-icon-180x180.png">
+  <link href="./favicon.ico" rel="icon">
+  
+  <title>Proyecto</title>  
+
+<link href="./Css/main.3da94fde.css" rel="stylesheet"></head>
+
+<body>
+
+ <!-- Add your content of header -->
+
+<header>
+  <nav class="navbar navbar-fixed-top navbar-default">
+    <div class="container">
+        <button type="button" class="navbar-toggle">
+          <span class="sr-only">Toggle navigation</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+        
+      <nav class="navbar-fullscreen" id="navbar-middle">
+        <button type="button" class="close" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        <ul class="navbar-fullscreen-links">
+          <li><a href="{{route('inicio')}}" title="">Inicio</a></li>
+          <li><a href="{{route('project')}}" title="">Proyecto</a></li>
+          <li><a href="{{route('about')}}" title="">Sobre mí</a></li>
+          <li><a href="{{route('components')}}" title="">Componentes</a></li>
+        </ul>
+
+        <div class="footer-container">
+           
+          <p><small>© Untitled | Website created with <a href="http://www.mashup-template.com/" title="Create website with free html template">Mashup Template</a>/<a href="https://www.unsplash.com/" title="Beautiful Free Images">Unsplash</a></small></p>
+          <p class="footer-share-icons">
+              <a href="https://www.twitter.com" class="fa-icon" title="">
+                  <i class="fa fa-twitter" aria-hidden="true"></i>
+              </a>
+              <a href="https://www.linkedin.com" class="fa-icon" title="">
+                  <i class="fa fa-instagram" aria-hidden="true"></i>
+              </a>
+              <a href="https://www.behance.com" class="fa-icon" title="">
+                  <i class="fa fa-behance" aria-hidden="true"></i>
+              </a>
+              <a href="https://www.vimeo.com" class="fa-icon" title="">
+                  <i class="fa fa-vimeo" aria-hidden="true"></i>
+              </a>
+          </p>
+                       
+        </div>
+
+
+      </nav> 
+    
+    </div>
+  </nav>
+</header>
+
+  
+<div class="hero-full-container background-image-container white-text-container" style="background-image:url('./images/img-08.jpg')">
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12">
+        <div class="hero-full-wrapper">
+          <div class="text-content  text-center">
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="section-container">
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12">
+        <div class="text-center section-container-spacer">
+          <h2 class="with-project-number"><span class="project-number">01</span>Night Photography</h2>
+        </div>
+        <div class="row section-container-spacer">
+          <div class="col-md-4">
+              <div class="section-container-spacer">
+                <h3>Torneo Copa Telmex</h3>
+                <p>Cuautitlán, Diciembre, 2021</p>
+              </div>
+
+              <h4>Directivos del club</h4>
+              
+              <ul class="list-unstyled">
+                <li>Rubén Dario</li> 
+                <li>Lucia Sosa</li>
+                <li>Nestor Sanchez</li>
+              </ul>
+              
+
+              <p>Cityzen circus Association</p>
+              <p>Menphis City Hall</p>
+          </div>
+          <div class="col-md-8">
+            <h3>Unión representa a Tecámac en Copa Telmex 2021</h3>
+            <p>Como orgullo, puedo decir que de las mayores experiencias en la vida que he tenido, fue participar en el torneo amateur más 
+              grande del mundo, llegar a ser subcampeón estatal es algo que no cualquier equipo ha logrado, siempre me sentiré 
+              orgulloso de representar a Unión 47.
+            </p>
+            Apesar de las adversidades económicas, se lorgró clasificar a la fase estatal, disputar un lugar en la final ante municipios con un 
+            gran nivel futbolistico tales como tepozotlán, zumpango, Nicolas romero, Texcoco, Toluca, entre otros más. 
+              <p> 
+            </p>
+          </div>
+        </div>
+
+
+        <img src="./images/img-03.jpg" alt="" class="img-responsive">
+        <p>Menphis city center</p>
+
+        <blockquote class="text-center large-spacing">
+            <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante."</p>
+            <small class="pull-right">Irina Martin</small>
+        </blockquote>
+        
+        <div class="row">
+            <div class="col-md-6">
+            <img src="./images/img-02.jpg" class="img-responsive" alt="">
+            <p>Menphis skyline</p>
+            </div>
+            <div class="col-md-6">
+            <img src="./images/img-01.jpg" class="img-responsive" alt="">
+            <p>Artefact Buildging</p>
+            </div>
+        </div>
+
+        <img src="./images/img-04.jpg" class="img-responsive" alt="">
+        <p>Menphis skyline</p>
+
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="navbar navbar-default navbar-fixed-bottom">
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12 text-center">
+        <ul class="navbar-nav nav">
+          <li>
+            <a href="" title="" class="project-nav left"><span class="project-number">01</span>Previous</a>
+          </li>
+        </ul>
+        <p class="h5 navbar-text">Night Photography <i class="fa fa-chevron-down arrow-down" aria-hidden="true"></i></p>
+
+        <ul class="navbar-nav nav navbar-right">
+          <li>
+            <a href="" title="" class="project-nav right"><span class="project-number">03</span>Next </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+
+
+
+<script>
+    document.addEventListener("DOMContentLoaded", function (event) {
+      navbarToggleSidebar();
+      closeMenuBeforeGoingToPage();
+      navActivePage();
+    });
+</script>
+ <script type="text/javascript" src="./Js/main.4c6e144e.js"></script></body>
+
+</html>
